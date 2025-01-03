679bdc9d19e7a9c2581f77334c6efd79b0822fe1
andreluiz2431 <alm28062001@gmail.com>
2025-01-02 22:35:34 -0300

    aDICIONANDO ROTAS cancel_scheduled_message, list_scheduled_messages, check_user_status

--- __main.py
+++ __main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.scheduler import shutdown_scheduler
-from app.routes import webhook, send_template_message, logs, schedule_message, send_instant_message, update_token, current_token, expiration_time
+from app.routes import cancel_scheduled_message, list_scheduled_messages, check_user_status, webhook, send_template_message, logs, schedule_message, send_instant_message, update_token, current_token, expiration_time
 
 app = FastAPI(title="Chatbot WhatsApp Scheduler")
 
 # Incluindo rotas
+app.include_router(cancel_scheduled_message.router)
+app.include_router(list_scheduled_messages.router)
+app.include_router(check_user_status.router)
 app.include_router(current_token.router)
 app.include_router(expiration_time.router)
 app.include_router(logs.router)
