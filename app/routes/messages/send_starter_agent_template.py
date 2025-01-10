1ec8d3375e587c88cb5e4854ac04752236f44845
joao-pedro-rdo <joaoprdo2.aluno@unipampa.edu.br>
2025-01-10 15:24:18 -0300

    tem bug usando a service

--- app/routes/messages/send_starter_agent_template.py
+++ app/routes/messages/send_starter_agent_template.py
@@ -1,46 +1,19 @@
 from fastapi import APIRouter, HTTPException
-from app.models.InstantMessageRequest import InstantMessageRequest
-import requests
-import logging
-from pydantic import BaseModel
-from app.models.SendStarterAgentTemplate import TemplateRequest  # Template da mensagem
-from app.config import ACCESS_TOKEN, WHATSAPP_API_URL
+from app.models.SendStarterAgentTemplate import TemplateRequest
+from app.services.send_starter_agent_service import send_starter_agent_template
 
 router = APIRouter()
 
 
 @router.post("/send-starter-agent")
-def send_starter_agent_template(request: TemplateRequest):
+def send_starter_agent(request: TemplateRequest):
     """
-    Endpoint para enviar o template `starter_agent` sem variáveis.
+    Endpoint para enviar o template `starter_agent`.
     """
-    headers = {
-        "Authorization": f"Bearer {ACCESS_TOKEN}",
-        "Content-Type": "application/json",
-    }
-    payload = {
-        "messaging_product": "whatsapp",
-        "to": request.recipient,
-        "type": "template",
-        "template": {
-            "name": "starter_agent",  # Nome do template
-            "language": {"code": "pt_BR"},  # Idioma configurado no template
-        },
-    }
-    # TODO Arurmar esse payload
-    # Log do payload para depuração
-    logging.info(f"Payload enviado para o WhatsApp: {payload}")
-
     try:
-        response = requests.post(WHATSAPP_API_URL, json=payload, headers=headers)
-        response.raise_for_status()
-        logging.info(
-            f"Template enviado com sucesso para {request.recipient}. Resposta: {response.json()}"
-        )
-        return {"status": "success", "response": response.json()}
-    except requests.exceptions.RequestException as e:
-        if e.response is not None:
-            logging.error(f"Erro ao enviar template: {e.response.text}")
-        raise HTTPException(
-            status_code=500, detail=f"Erro ao enviar template: {e.response.text}"
-        )
+        response = send_starter_agent_template(request.recipient)
+        return {"status": "success", "response": response}
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
